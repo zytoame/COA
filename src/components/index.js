c69c37ab0267129d2a18d62faf3153dec929086b
zytoame <129404301+zytoame@users.noreply.github.com>
2025-12-03 20:29:29 +0800

    feat: AI 生成文件 version 21

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export * from './EditModal.jsx';
 export * from './DetectionDataCard.jsx';
 export * from './DetailModal.jsx';
-export * from './BatchAuditSection.jsx';
 export * from './SignaturePad.jsx';
--- src/configs/routers.ts
+++ src/configs/routers.ts
@@ -1,16 +1,8 @@
-import HOME from '../pages/home.jsx';
-import QUERY_REPORT from '../pages/query-report.jsx';
 import BATCH_AUDIT from '../pages/batch-audit.jsx';
 import MAIN from '../pages/main.jsx';
 import UNQUALIFIED_REPORTS from '../pages/unqualified-reports.jsx';
 import QUERY_REPORTS from '../pages/query-reports.jsx';
 export const routers = [{
-  id: "home",
-  component: HOME
-}, {
-  id: "query-report",
-  component: QUERY_REPORT
-}, {
   id: "batch-audit",
   component: BATCH_AUDIT
 }, {
